3b78717b4eb540cef9066f3ace4aa37f32b68c4e
Alex Alemi <alexalemi@gmail.com>
2022-12-03 00:49:18 -0500

    advent-2022-03

---
+++ notebooks/advent-2022-03.clj
@@ -0,0 +1,42 @@
+;; # 🎄 Advent Of Code 2022 - Day 3
+;;
+(ns p03
+  (:require [clojure.string :as str]
+            [clojure.set :as set]))
+
+(def data-string (slurp "input/advent-2022-03.txt"))
+
+(defn lower-case? [s]
+  (<= (int \a) (int s) (int \z)))
+
+(defn score [x]
+  (if (lower-case? x)
+      (inc (- (int x) (int \a)))
+      (+ 26 (inc (- (int x) (int \A))))))
+
+(defn split-at-half [s]
+  (map set (split-at (quot (count s) 2) s)))
+
+(defn intersect [x]
+  (apply set/intersection x))
+
+;; ## Part 1
+
+(def ans1
+  (->> (str/split-lines data-string)
+       (map (comp score first intersect split-at-half seq))
+       (reduce +)))
+
+;; ## Part 2
+
+(def ans2
+  (->> (str/split-lines data-string)
+       (map (comp set seq))
+       (partition 3)
+       (map (comp score first intersect))
+       (reduce +)))
+
+
+(defn -main [& _]
+  (println "Answer1:" ans1)
+  (println "Answer2:" ans2))
